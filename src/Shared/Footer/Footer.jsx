97527c9e6cf5acbffb2a0d1a122bfcaadfa3896c
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2025-01-15 13:01:16 +0600

    Footer section has been almost done

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,8 +1,83 @@
+import Container from "../../components/Container/Container";
+import footerLogo from "../../assets/images/logo.png";
+import { Link, NavLink } from "react-router-dom";
+import {
+  FaFacebook,
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <div>
-      <h2>This is the footer</h2>
-    </div>
+    <Container width="1560px">
+      <footer>
+        <div className="flex flex-row justify-between font-poppins items-center">
+          {/* This is the logo Section */}
+          <img src={footerLogo} alt="" />
+          {/* This is the Navigation Section */}
+          <nav>
+            <ul className="flex space-x-[55px]">
+              <li>
+                <NavLink to="/" className={"navClass"}>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/about" className={"navClass"}>
+                  About Us
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/services" className={"navClass"}>
+                  Our Services
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact" className={"navClass"}>
+                  Contact Us
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          {/* This is the social icons section */}
+          <div className="flex flex-row gap-[34px]">
+            <a
+              href="https://twitter.com/yourprofile"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-blue-500 transition-colors duration-300"
+            >
+              <FaTwitter />
+            </a>
+            <a
+              href="https://facebook.com/yourprofile"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-blue-600 transition-colors duration-300"
+            >
+              <FaFacebookF />
+            </a>
+            <a
+              href="https://facebook.com/yourprofile"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-pink-500 transition-colors duration-300"
+            >
+              <FaInstagram />
+            </a>
+          </div>
+        </div>
+        {/* This is the copyright section */}
+        <div className="flex flex-row justify-between navbarBottom">
+          <h1>© Copyright 2025, All Rights Reserved</h1>
+          <div className="flex flex-row gap-10">
+            <p>Privacy Policy</p>
+            <p>Terms & Conditions</p>
+          </div>
+        </div>
+      </footer>
+    </Container>
   );
 };
 
