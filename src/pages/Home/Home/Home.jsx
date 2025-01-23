d4a314d03eec496acf21539b8f98b1acf36ddb07
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2025-01-23 16:34:32 +0600

    Navbar and banner text has been done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -13,7 +13,24 @@ const Home = () => {
       <Helmet>
         <title>Ayos || Home</title>
       </Helmet>
-      {/*  */}
+      <Banner
+        title={
+          <>
+            Expert Assistance, Anytime, <br /> Anywhere Ayos
+          </>
+        }
+        subtitle={
+          <>
+            Connect with trusted professionals for all your home needs—cleaning,
+            repairs,
+            <br />
+            maintenance, and beyond. Post your job today and experience fast,
+            reliable service <br /> with a hassle-free process!
+          </>
+        }
+        backgroundImage="https://i.postimg.cc/MTvfGNm0/banner-Img.png"
+        gradient="linear-gradient(90deg, rgba(9, 25, 64, 0.80) -0.85%, rgba(23, 64, 166, 0.00) 99.73%)"
+      />
       <AyosPlatform />
       <ConnectWithBestHome />
       <CardSection />
