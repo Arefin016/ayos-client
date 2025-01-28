4c9ac56ab1f83953369b949c5f91b7aab73e7b77
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2025-01-28 15:14:49 +0600

    Download app almost responsive has been done

--- src/components/DownloadApp/DownloadApp.jsx
+++ src/components/DownloadApp/DownloadApp.jsx
@@ -34,12 +34,12 @@ const DownloadApp = () => {
 
   return (
     <Container width="1560px">
-      <section className="bg-[#EEF3FF] rounded-[32px] mb-[93px] mt-5 xs:mt-5 sm:mt-5 md:mt-5 lg:mt-5 xl:mt-5 2xl:mt-10 3xl:mt-20">
-        <div className="flex flex-col xs:flex-col sm:flex-col md:flex-col lg:flex-col xl:flex-row 2xl:flex-row 3xl:flex-row gap-5 items-center">
-          <div className="ml-4 xs:ml-4 sm:ml-4 md:ml-4 lg:ml-8 xl:ml-10 2xl:ml-[51px] 3xl:ml-[51px]">
+      <section className="bg-[#EEF3FF] rounded-[32px] mb-[93px] mt-5 xs:mt-5 sm:mt-5 md:mt-5 lg:mt-5 xl:mt-5 2xl:mt-10 3xl:mt-20 mx-3 xs:mx-5 sm:mx-5 md:mx-5 lg:mx-5 xl:mx-5 2xl:mx-5 3xl:mx-5 4xl:mx-0 px-1 sm:px-3 md:px-3 lg:px-3 xl:px-0 2xl:px-0 3xl:px-0 4xl:px-0">
+        <div className="flex flex-col xs:flex-col sm:flex-col md:flex-col lg:flex-col xl:flex-col 2xl:flex-row 3xl:flex-row gap-5 xs:gap-5 sm:gap-5 md:gap-5 lg:gap-5 xl:gap-10 2xl:gap-5 3xl: items-center">
+          <div className="ml-4 xs:ml-4 sm:ml-4 md:ml-4 lg:ml-2 xl:ml-10 2xl:ml-[51px] 3xl:ml-[51px]">
             <h2
               data-aos="fade-up"
-              className="text-secondaryButton text-[28px] xs:text-[28px] sm:text-[30px] md:text-[35px] lg:text-[45px] xl:text-[45px] 2xl:text-[56px] 3xl:text-[56px] font-poppins w-full xs:w-full sm:w-full md:w-full lg:w-full xl:w-full 2xl:w-[650px] 3xl:w-[650px] font-semibold"
+              className="text-secondaryButton text-[28px] xs:text-[28px] sm:text-[30px] md:text-[35px] lg:text-[45px] xl:text-[45px] 2xl:text-[56px] 3xl:text-[56px] font-poppins w-[300px] xs:w-[340px] sm:w-[460px] md:w-[550px] lg:w-[650px] xl:w-[650px] 2xl:w-[650px] 3xl:w-[650px] font-semibold"
             >
               {data?.data.title}
             </h2>
@@ -54,7 +54,7 @@ const DownloadApp = () => {
             <div
               data-aos="fade-up"
               data-aos-delay=" 200"
-              className="flex flex-row gap-4 mt-[50px]"
+              className="flex flex-row gap-4 mt-[50px] pb-5 xs:pb-5 sm:pb-5 md:pb-5 lg:pb-5 xl:pb-0 2xl:pb-0 3xl:pb-0"
             >
               <Link to={data?.data?.button_two_url}>
                 <img src={data?.data?.button_one_image} alt="Play Store" />
@@ -67,7 +67,7 @@ const DownloadApp = () => {
           {/* This is the image section */}
           <div data-aos="zoom-in ">
             <img
-              className="w-full xs:w-full sm:w-full md:w-full lg:w-full xl:w-full 2xl:w-full 3xl:w-[752px] h-full xs:h-full sm:h-full md:h-full lg:h-auto xl:h-full 2xl:h-full 3xl:h-[800px] object-cover"
+              className="w-[300px] xs:w-[340px] sm:w-[460px] md:w-[560px] lg:w-[740px] xl:w-[752px] 2xl:w-[752px] 3xl:w-[752px] 4xl:w-[752px] h-[300px] xs:h-[340px] sm:h-[460px] md:h-[560px] lg:h-[740px] xl:h-[800px] 2xl:h-[800px] 3xl:h-[800px] 4xl:h-[800px] object-cover"
               src={data?.data?.image}
               alt="downloadApp"
             />
